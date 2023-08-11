07f27143f726c5a5bcdb0a1b5a9504b6c1b939e4
faith-ye <m202171620@hust.edu.cn>
2023-08-11 19:52:09 +0800

    完成登录功能

--- hm-dianping/src/main/java/com/hmdp/controller/UserController.java
+++ hm-dianping/src/main/java/com/hmdp/controller/UserController.java
@@ -1,13 +1,12 @@
 package com.hmdp.controller;
 
-
-import cn.hutool.core.util.RandomUtil;
 import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.UserInfo;
 import com.hmdp.service.IUserInfoService;
 import com.hmdp.service.IUserService;
-import com.hmdp.utils.RegexUtils;
+import com.hmdp.utils.UserHolder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
@@ -38,20 +37,7 @@ public class UserController {
      */
     @PostMapping("code")
     public Result sendCode(@RequestParam("phone") String phone, HttpSession session) {
-        // TODO 发送短信验证码并保存验证码
-        // 校验手机号
-        if(RegexUtils.isPhoneInvalid(phone)){
-            // 不符合，返回错误信息
-            return Result.fail("手机号格式错误");
-        }
-        // 符合，生成验证码
-        String code = RandomUtil.randomNumbers(6);
-
-        // 保存验证码到session
-        session.setAttribute("code", code);
-        // 发送验证码
-        log.debug("发送短信验证码成功，验证码：{}", code);
-        return Result.ok();
+        return userService.sendCode(phone, session);
     }
 
     /**
@@ -61,7 +47,7 @@ public class UserController {
     @PostMapping("/login")
     public Result login(@RequestBody LoginFormDTO loginForm, HttpSession session){
         // TODO 实现登录功能
-        return Result.fail("功能未完成");
+        return userService.login(loginForm, session);
     }
 
     /**
@@ -77,7 +63,8 @@ public class UserController {
     @GetMapping("/me")
     public Result me(){
         // TODO 获取当前登录的用户并返回
-        return Result.fail("功能未完成");
+        UserDTO userDTO = UserHolder.getUser();
+        return Result.ok(userDTO);
     }
 
     @GetMapping("/info/{id}")
